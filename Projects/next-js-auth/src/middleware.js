4d4f03ee4abf8e6d07d19f76a0e3c662419d01b9
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-09-23 18:54:32 +0530

    Delete Projects/next-js-auth directory

--- Projects/next-js-auth/src/middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-export async function middleware(req){
-    const cookie=req.cookies.get('token');
-
-    if(!cookie && req.url.includes("/profile")){
-        return NextResponse.redirect(new URL('/login',req.url))
-    }
-}
